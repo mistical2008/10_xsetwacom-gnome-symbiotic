c72f9805cdd0f648cff8e4bc40cd18830d6e87bd
Evgeniy <etalko@gmail.com>
2018-10-16 18:56:52 +0300

    Small changes

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 USER_HOME=$(eval echo ~${SUDO_USER})
+# Write user home path to *.service
 
 # Copy files to destinations
 mkdir $USER_HOME/.config/xsetwacom-gnome-symbiotic/
 chown evgeniy.evgeniy $USER_HOME/.config/xsetwacom-gnome-symbiotic/
 cp xsetwacom-gnome-symbiotic_setup.sh $USER_HOME/.config/xsetwacom-gnome-symbiotic/setup.sh
 cp xsetwacom-gnome-symbiotic.rules /etc/udev/rules.d/
-cp xsetwacom-gnome-symbiotic.service /etc/systemd/system/
+cp xsetwacom-gnome-symbiotic.service xsetwacom-gnome-symbiotic.service1
+sed -i "s,1USER_HOME1,$USER_HOME,g" "xsetwacom-gnome-symbiotic.service1"
+mv xsetwacom-gnome-symbiotic.service1 /etc/systemd/system/xsetwacom-gnome-symbiotic.service
 
 # Run commands
 chmod +x $USER_HOME/.config/xsetwacom-gnome-symbiotic/setup.sh
